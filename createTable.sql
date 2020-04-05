a9e159b5e562941e86b6245b93d7ca63c9bf724a
Christian Gang Liu <chris.gang.liu@outlook.com>
2020-04-05 20:25:04 -0300

    fixing

--- createTable.sql
+++ createTable.sql
@@ -11,3 +11,10 @@ CREATE TABLE IF NOT EXISTS `part` (
    PRIMARY KEY  (`part_no`)
 
 );
+
+INSERT INTO `part` SET `part_no` = 0, `part_desc` = `engine`;
+INSERT INTO `part` SET `part_no` = 1, `part_desc` = `windshield`;
+INSERT INTO `part` SET `part_no` = 2, `part_desc` = `wheel`;
+INSERT INTO `part` SET `part_no` = 3, `part_desc` = `gps`;
+INSERT INTO `part` SET `part_no` = 4, `part_desc` = `radio`;
+commit;
